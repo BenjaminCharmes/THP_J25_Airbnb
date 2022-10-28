a43214ef8c2e0d5512e9d3dd92f83b7c8b258b29
BenjaminCharmes <benjamin.charmes@gmail.com>
2022-10-28 14:47:02 +0100

    Last commit

--- db/migrate/20221028102741_create_listings.rb
+++ db/migrate/20221028102741_create_listings.rb
@@ -6,6 +6,8 @@ class CreateListings < ActiveRecord::Migration[7.0]
       t.text :description
       t.boolean :has_wifi
       t.text :welcome_message
+      t.references :admin, index: true
+      t.belongs_to :city, index: true
 
       t.timestamps
     end
--- db/migrate/20221028102826_create_reservations.rb
+++ db/migrate/20221028102826_create_reservations.rb
@@ -3,6 +3,8 @@ class CreateReservations < ActiveRecord::Migration[7.0]
     create_table :reservations do |t|
       t.datetime :start_date
       t.datetime :end_date
+      t.references :guest, index: true
+      t.belongs_to :listing, index: true
 
       t.timestamps
     end
